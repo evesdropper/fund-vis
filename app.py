37a91a727dc87d0e9b8b06077b57b13540129da9
randomrevised <r_evelynkoo@berkeley.edu>
2022-05-27 16:12:34 -0700

    new template

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+import src.scraper as scraper
+import src.fund as fund
+from flask import Flask, render_template, Response
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Tanki Fund 2022"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8080, debug=True)
