fd0b592c834397fe0cff4d21e6f64592b62aa512
randomrevised <r_evelynkoo@berkeley.edu>
2022-05-27 11:50:30 -0700

    first working model?

--- src/scraper.py
+++ src/scraper.py
@@ -1,15 +1,20 @@
  # necessary imports - scraping
+import os
 import requests
 from bs4 import BeautifulSoup
-import pickle
 
 # data analysis
 import datetime
 import numpy as np
-# import cvxpy as cp - used for LPs but we don't have any now
 import matplotlib.pyplot as plt
 
-# utility - store fund entries
+# big capital letters
+CWD = os.getcwd()
+SAVE_DIR = os.path.join(CWD, "saved")
+
+URL = "https://tankionline.com/pages/tanki-birthday-2022/" # when new fund website
+
+# fund entries
 class FundEntry():
     
     def __init__(self, value):
@@ -21,3 +26,10 @@ class FundEntry():
     
     def __str__(self):
         return f"Fund at {self.time}: {self.value}"
+
+
+def get_entry():
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, "html.parser")
+    fund = soup.find_all("span", class_="ms-3")
+    return fund[0].text
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 # utility functions
+import pickle, os
 
 def save_entry(obj):
     with open('fund.txt', 'wb') as handle:
@@ -8,3 +9,11 @@ def load_entry():
     with open('fund.txt', 'rb') as handle:
         load = pickle.load(handle)
     return load
+
+# typical gitlet behavior
+def join(path, file):
+    return os.path.join(path, file)
+
+def clean_dirs(dir):
+    for file in os.listdir(dir):
+        os.remove(join(dir, file))
