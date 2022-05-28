6634f3659d199190553a3b5281202c42b33292e2
randomrevised <r_evelynkoo@berkeley.edu>
2022-05-28 00:48:28 -0700

    Almost Hourly Update

--- cron.sh
+++ cron.sh
@@ -3,7 +3,7 @@ python3 src/fund.py cron
 
 message=$(date '+%Y-%m-%d %H:%M:%S')
 git add .
-git commit -m "$message"
+git commit -m "add entry at ${message}"
 git subtree push --prefix src heroku main
 
 echo "cron commit at ${message}" >> cronlog.txt
