1ec87b14ad6a9f0e2dcba6291eaee089caf68fa4
randomrevised <r_evelynkoo@berkeley.edu>
2022-05-27 22:56:25 -0700

    bruh i cannot webdev

--- src/fund.py
+++ src/fund.py
@@ -6,16 +6,20 @@ from scraper import FundEntry
 
 def cmd_input(args):
     command = args[0]
-    match command:
-        case "--help":
-            print("You're not a cron job; don't ask for help.")
-        case "cron":
-            scraper.get_entry()
-        case "reset":
-            scraper.reset()
-        case _:
-            print(f"No match found for given argument. {exception_text}")
-            return
+    if command == "cron":
+        scraper.get_entry()
+    else:
+        print("Imposter Alert")
+    # match command:
+    #     case "--help":
+    #         print("You're not a cron job; don't ask for help.")
+    #     case "cron":
+    #         scraper.get_entry()
+    #     case "reset":
+    #         scraper.reset()
+    #     case _:
+    #         print(f"No match found for given argument. {exception_text}")
+    #         return
 
 exception_text = "Try 'python fund.py --help' to search for a given command."
 
