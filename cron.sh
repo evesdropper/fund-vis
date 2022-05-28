e29374253387048174ee399ff709c5e6ff456a30
randomrevised <r_evelynkoo@berkeley.edu>
2022-05-28 00:40:04 -0700

    add some random cron stuff

--- cron.sh
+++ cron.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
-python src/fund.py cron
+python3 src/fund.py cron
 
 message=$(date '+%Y-%m-%d %H:%M:%S')
 git add .
 git commit -m "$message"
 git subtree push --prefix src heroku main
+
+echo "cron commit at ${message}" >> cronlog.txt
+
