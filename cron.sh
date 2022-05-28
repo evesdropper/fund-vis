2aa9a2165abb9c3d884be25c3c98d3b2d7422618
randomrevised <r_evelynkoo@berkeley.edu>
2022-05-28 10:46:39 -0700

    add entry at 2022-05-28 10:46:38

--- cron.sh
+++ cron.sh
@@ -7,8 +7,4 @@ git add .
 git commit -m "add entry at ${message}"
 echo "cron commit at ${message}" >> cronlog.txt
 
-git subtree push --prefix src heroku main
-
-fin=$(date '+%Y-%m-%d %H:%M:%S')
-echo "commit complete at ${fin}" >> cronlog.txt
 
