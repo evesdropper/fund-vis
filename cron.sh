ee5f425466d90f608e66785d23a57d202f4d80cb
randomrevised <r_evelynkoo@berkeley.edu>
2022-05-28 17:32:46 -0700

    forgor :skull:

--- cron.sh
+++ cron.sh
@@ -4,7 +4,7 @@ cd src
 python3 fund.py cron
 cd ..
 message=$(date '+%Y-%m-%d %H:%M:%S')
-git add .
+git add src/saved/fund.txt
 git commit -m "add entry at ${message}"
 echo "cron commit at ${message}" >> cronlog.txt
 cd src
